0ba655844235bccb001edec628b66ffc3e42c9a9
Krzysztof Wolkowycki <wolkowycki.k@gmail.com>
2021-11-11 11:35:42 +0100

    function timer

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
 const btnMain = document.querySelector('button.main');
 const btnReset = document.querySelector('button.reset');
-
+const panel = document.querySelector('div.time div');
 let active = false;
+let time = 0;
+let idI;
 
 const start = () => {
     if (!active) {
         active = !active;
         btnMain.textContent = 'Stop';
-        setInterval(timer, 1000);
+        idI = setInterval(timer, 10);
     } else {
         active = !active;
         btnMain.textContent = 'Start';
+        clearInterval(idI);
     }
+}
+const timer = ()=>{
+    if(active = true){
+        time++;
+    }
+    panel.textContent = (time/100).toFixed(2);
 }
 
 
